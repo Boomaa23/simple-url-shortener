3ef4a066aea90cf7f8aa89482df4627f4f28cc47
Boomaa23 <cmo93003@yahoo.com>
2021-07-22 00:54:23 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# simple-url-shortener
+
+A simple lightweight URL shortener. Currently used on https://url.ncocdn.cf/.
+
+## Features
+- Lightweight: Uses a JSON file to store URLs instead of a database
+- Customizable: Random links (from a preset charset) or user-enterable
+- Tracking: Can track user IP when shortened links used (with timestamp)
---
+++ index.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Link Shortener</title>
+  <style>
+    input { padding: 5px; margin: 5px; }
+    label { font-size: 14px; }
+    .footer {position: fixed; text-align: center; bottom: 0px; width: 100%; background-color:white;font-size:10px;}
+    .center { text-align: center; }
+    #title { font-weight: bold; font-size: 22px; }
+    .nostyle { text-decoration:none; color: black; }
+    body { font-family: sans-serif; animation: 2s popIn; }
+    #remove, #track { margin: 10px 0 10px 10px; }
+    <?php
+    if (isset($_GET["dest"])) {
+      echo '
+      @keyframes popIn {
+        0% { opacity: 0; }
+        99% { opacity: 0; }
+        100% { opacity: 1; }
+      }';
+    }
+    ?>
+  </style>
+</head>
+<body>
+  <div class="center" id="main">
+    <div id="title">Link Shortener</div>
+    <?php
+    $linksJson = json_decode(file_get_contents("links.json"), true);
+    $clicksJson = json_decode(file_get_contents("clicks.json"), true);
+    if (isset($_POST["link"])) {
+      if (isset($_POST["remove"])) {
+        $fn = $_POST["custom"];
+        unset($linksJson[$fn]);
+        unset($clicksJson[$fn]);
+        $urlFn = "https://url.ncocdn.cf/" . $fn;
+
+        echo 'Removed Shortened Link: <a href="' . $urlFn . '">' . $urlFn . '</a><br /><br />';
+        echo '<button><a href="https://ncocdn.cf/url/" class="nostyle">Shorten Another</a></button>';
+      } else {
+        $length = 6; //length of final shortened random url
+        $randChars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+        $rand = substr(str_shuffle(str_repeat($randChars, ceil($length / strlen($randChars)))), 1, $length);
+        if (isset($_POST["custom"]) && trim($_POST["custom"]) !== "") {
+          $rand = $_POST["custom"];
+        }
+        $url = $_POST["link"];
+        $linksJson[$rand] = $url;
+        if (isset($_POST["track"])) {
+          $clicksJson[$rand] = array();
+        }
+
+        $shortUrl = 'https://url.ncocdn.cf/' . $rand;
+
+        echo 'Shortened Link: <a href="' . $shortUrl . '">' . $shortUrl . '</a><br /><br />';
+        echo '<button><a href="https://ncocdn.cf/url/" class="nostyle">Shorten Another</a></button>';
+      }
+      file_put_contents("links.json", json_encode($linksJson, JSON_PRETTY_PRINT));
+      file_put_contents("clicks.json", json_encode($clicksJson, JSON_PRETTY_PRINT));
+    } else if (isset($_GET["dest"])) {
+      $url;
+      if (array_key_exists($_GET["dest"], $linksJson)) {
+        $url = $linksJson[$_GET["dest"]];
+      } else if (empty($_GET["dest"])) {
+        $url = "https://ncocdn.cf/url/";
+      }
+      if (isset($url)) {
+        $ip = 'IP';
+        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+          $ip = $_SERVER['HTTP_CLIENT_IP'];
+        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+          $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        } else {
+          $ip = $_SERVER['REMOTE_ADDR'];
+        }
+        if (!empty($_GET["dest"]) && array_key_exists($_GET["dest"], $clicksJson) && $ip !== "70.191.89.252") {
+          $clicksJson[$_GET["dest"]][date('c', time())] = $ip;
+          file_put_contents("clicks.json", json_encode($clicksJson, JSON_PRETTY_PRINT));
+        }
+
+        echo '<!DOCTYPE html><html><head><meta http-equiv="refresh" content="0; url=\'' . $url . '\'" /></head><body>' .
+        '<p>Please follow this link: <a href="' . $url . '">' . $url . '</a></p></body></html>';
+      } else {
+        echo 'Could not find a URL redirect for "' . $_GET["dest"] . '"<br /><br />';
+        echo '<button><a href="https://ncocdn.cf/url/" class="nostyle">Shorten a Link</a></button>';
+      }
+    } else {
+      echo '
+      <form method="post">
+        <input type="text" name="link" placeholder="Link" /><br />
+        <input type="text" name="custom" placeholder="Custom URL" /><br />
+        <input type="checkbox" name="remove" id="remove"/>
+        <label for="remove">Remove</label>
+        <input type="checkbox" name="track" id="track" checked="checked" />
+        <label for="track">Track</label><br />
+        <input type="submit" value="       Shorten       " />
+      </form>';
+    }
+    ?>
+  </div>
+</body>
+<div class="footer">Copyright &copy; 2020-2021 Boomaa23. All Rights Reserved.</div>
+<script type="text/javascript">
+  function center() {
+    document.getElementById("main").style.paddingTop = (document.documentElement.clientHeight * 0.35) + "px";
+    setTimeout(center, 1000);
+  }
+  center();
+</script>
+</html>
